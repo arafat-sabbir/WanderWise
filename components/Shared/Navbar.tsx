22c76d2a73808d3f8368fe2c79ca386aa14a9738
arafat-sabbir <arafatshabbir8@gmail.com>
2024-10-18 19:17:48 +0600

    Update Website Logo

--- components/Shared/Navbar.tsx
+++ components/Shared/Navbar.tsx
@@ -27,11 +27,10 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
-          <div className="flex-shrink-0">
-            <Link href="/" className="text-xl font-bold">
-              Logo
-            </Link>
-          </div>
+          <Link href={"/"} className="flex-shrink-0 flex  items-center gap-2 text-lg font-semibold">
+            <Image src="/wise.png" alt="Logo" height={40} width={40} />
+            <h1>Wander Wise</h1>
+          </Link>
 
           {/* Navigation Links - hidden on mobile */}
           <div className="hidden md:block">
